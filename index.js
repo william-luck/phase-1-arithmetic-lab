875f5803fcb427af73d1d42024857c7797d88c94
William Luck <luck.william.h@gmail.com>
2022-03-14 13:46:05 -0500

    commit tree 44e1bd6fa1e1f07c11ba2ab96acb88ae4ebd5873

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// test one
+let num1 = 2; 
+let num2 = 31;
+const multiply = (num1 * num2);
 
-// Write your code here
+// test two 
+const random = Math.floor(Math.random() * 10) + 1;
+
+// test three 
+let num3 = 12;
+let num4 = 8;
+const mod = (num3 % num4);
+
+// test four 
+
+const max = Math.max(1, 5, 6, 7, 8, 11, 19, 20);
+
